3561244894716de3725ebba6cedc4311a6005bce
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-04-17 10:35:53 -0400

    starting implementation for transcripter. Loads an audio files from data/samples with either librosa or pydub. still undecided on final method. Will transcript the file using openAI whisper. Added in imports needed

---
+++ backend/transcription/transcripter.py
@@ -0,0 +1,47 @@
+import os 
+import librosa
+import pydub
+import numpy as np
+import torch
+import pandas as pd
+import whisper
+import torchaudio
+
+from tqdm.notebook import tqdm
+
+# pydub import
+from pydub import AudioSegment
+
+
+
+
+# Dir where samples are kept
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+
+samples_dir = os.path.join(root, "data", "samples")
+
+file_name = ''
+
+
+# Librosa implementation
+def librosa_load_file(file_name):
+  audio_data, sampling_rate = librosa.load(os.join(samples_dir, file_name))  # Can be other formats too
+  print(f"Sampling Rate: {sampling_rate} Hz")
+  print(f"Audio Data Shape: {audio_data.shape}")
+  print(f"Data Type: {audio_data.dtype}")
+
+# Pydub implementation
+def pydub_load_file(file_name):
+  audio = AudioSegment.from_file(os.join(samples_dir, file_name), format='m4a') # Specify format if needed
+  audio_data = audio.get_array_of_samples()
+  sampling_rate = audio.frame_rate
+  print(f"Sampling Rate: {sampling_rate} Hz")
+  print(f"Audio Data Length: {len(audio_data)}")
+  print(f"Data Type: {audio_data.dtype}")
+
+
+
+
+
+
+# Transcripting with openAI whisper
